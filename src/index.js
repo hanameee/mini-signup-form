48360b7986c5b6b7a16631c78da270cfdb4e2b66
lcqff <71930280+lcqff@users.noreply.github.com>
2023-06-12 16:58:27 +0900

    feat: 새로고침시 id input focus - 화면이 로딩 될때마다 input에 focus가 되어있도록 한다.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // TODO: 이 곳에 정답 코드를 작성해주세요.
+
+// 요구사항 1. autofocus
+// 페이지가 로드 된 시점에 ID 입력 창에 Focus가 되어 있어야 합니다.
+// HTML의 input 속성에 autoFocus를 삽입하는 방식으로도 구현가능
+const $id = document.getElementById('id')
+
+window.addEventListener('load', () => $id.focus())
